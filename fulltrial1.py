73bfded74154da40d163451b6a40bd614f2092de
devdj16 <devdjcvv16@gmail.com>
2021-05-23 11:51:52 +0530

    new function added

--- fulltrial1.py
+++ fulltrial1.py
@@ -1,34 +1,29 @@
 #FUNCTION TO FIND THE DENSITY OF TRAFFIC IN EACH LANE
 #RETURNS A DICTIONARY
 def Find_density():
-    density = {}
-    sorted_values = sorted(density.values()) # Sort the values
-    sorted_density = {}
+    density = {'lane-1': 13, 'lane-2': 17, 'lane-3': 10, 'lane-4': 13}
+    sorted_density = sorted(density.items(), key=lambda x: x[1], reverse=True)
 
-    for i in sorted_values:
-       for k in density.keys():
-            if density[k] == i:
-                sorted_density[k] = dict1[k]
-                break
+
     return sorted_density        
 
 
 #FUNCTION TO SEND GREEN SIGNAL ACCORDING TO PRIORITY
 def Check_density():
     sorted_density=Find_density()
-    max=7
-    for i in sorted_density.values():
-        #find key
-        lane=#
-        if i==0:
+    max=10
+    for i in sorted_density:
+        lane =i[0]
+        if i[1]==0:
             Traffic_Light(lane,11)
-        elif i>max:
+        elif i[1]>max:
             Traffic_light(lane,36)
         else:
             Traffic_light(lane,26)
             
 
 #FUNCTION TO DISPLAY THE STATE OF EACH TRAFFIC LIGHT IN A CYCLE 
-def Traffic_light():
-    if lane==lane1:
-       print() 
+def Traffic_light(lane,time):
+    if lane==lane-1:
+       print("Lane-1")
+
